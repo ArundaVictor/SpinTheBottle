9c6493cb4e75b30c2832ab1662bda7b8454f6088
Arunda Victor <victoroyugi31@gmail.com>
2019-07-10 01:02:39 +0300

    added spin rotation

--- app/src/main/java/com/example/spin/MainActivity.java
+++ app/src/main/java/com/example/spin/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.spin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottle) ImageView spinBottle;
+    private Random random = new Random();
+    private int lastDir;
+    private boolean spinning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    public void spin(View view) {
+        if (!spinning) {
+            int newDir = random.nextInt(1800);
+
+            //to get the middle of the imageView
+            float pivotX = spinBottle.getWidth() / 2;
+            float pivotY = spinBottle.getHeight() / 2;
+
+            //rotating anaimation
+            Animation rotate = new RotateAnimation(lastDir, newDir, pivotX, pivotY);
+            rotate.setDuration(5000);
+            rotate.setFillAfter(true);
+
+            lastDir = newDir;
+            spinBottle.startAnimation(rotate);
+        }
+
     }
 }
